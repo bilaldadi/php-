726f97c373f83892f440ba8739dd4cea4732e7f3
bilaldadi <48449447+bilaldadi@users.noreply.github.com>
2021-06-27 03:01:45 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,29 @@
+<?php
+
+	$server = "sql209.epizy.com";
+	$username = "epiz_28910590";
+	$password = "4BzeuuariYPLj";
+	$dbname = "epiz_28910590_bilalsStore";
+
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	$conn = new mysqli($server,$username,$password,$dbname);
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
